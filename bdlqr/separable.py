Fix the sign mistake in linear algebra

bdlqr/separable.py
@@ -1,3 +1,4 @@
+import warnings
 import math
 from functools import partial
 from collections import namedtuple, deque
@@ -170,40 +171,18 @@ def solve_admm(slsys, y0, x0, traj_len, ε=1e-4, ρ=1, max_iter=10):
         #  and xhₜ =  [ xₜ ]
         #             [ 1  ]
         x0h    = np.hstack((x0, 1))
-        E_vs    = [np.hstack((E, (-v + w/ρ).reshape(-1,1)))
+        E_vs   = [np.hstack((E, (-v + w/ρ).reshape(-1,1)))
                    for v, w in zip(vs, ws)]
         Qxs    = [0.5 * ρ * E_vt.T.dot(E_vt)    for E_vt in E_vs]
         Axh     = np.eye(x0h.shape[0])
         Axh[:-1, :-1] = Ax
         Buh     = np.vstack((Bu, np.zeros((1, Bu.shape[1]))))
-        # x_sys   = LinearSystem(Axh, Buh,
-        #                        Qxs[:-1], np.zeros(Qxs[0].shape[0]),
-        #                        R, np.zeros(R.shape[0]),
-        #                        Qxs[-1], np.zeros(Qxs[-1].shape[0]),
-        #                        T)
-        # xhs_new, us_new, usmin = x_sys.solve(x0h, T, return_min=True)
-        Ps = deque([Qxs[T-1]])
-        Ks = deque([])
-        for t in reversed(range(T-1)):
-            Ptp1 = Ps[0]
-            # Kt = np.linalg.solve(R + Buh.T.dot(Ptp1).dot(Buh), Buh.T.dot(Ptp1).dot(Axh))
-            # Pt = Qxs[t] + Axh.T.dot(Ptp1).dot(Axh) - Axh.T.dot(Ptp1).dot(Buh).dot(Kt)
-            Pt, _, Kt, _ = affine_backpropagation(
-                Qxs[t], np.zeros(Qxs[t].shape[1]),
-                R, np.zeros(R.shape[1]),
-                Axh, Buh, Ps[0], np.zeros(Ps[0].shape[1]))
-            Ks.appendleft(Kt)
-            Ps.appendleft(Pt)
-        xhs_new = [x0h]
-        us_new = []
-        for t in range(T-1):
-            xt = xhs_new[t]
-            us_new.append(Ks[t].dot(xt))
-            ut = us_new[t]
-            xhs_new.append(Axh.dot(xt) + Buh.dot(ut))
-
-        usmin = x0h.T.dot(Ps[0]).dot(x0h)
-        LOG.debug("usmin: {:.03f}".format(usmin))
+        x_sys   = LinearSystem(Axh, Buh,
+                               Qxs[:-1], np.zeros(Qxs[0].shape[0]),
+                               R, np.zeros(R.shape[0]),
+                               Qxs[-1], np.zeros(Qxs[-1].shape[0]),
+                               T)
+        xhs_new, us_new, usmin = x_sys.solve(x0h, T, return_min=True)
         xs_new = [xh[:-1] for xh in xhs_new]
 
         ###
@@ -215,44 +194,20 @@ def solve_admm(slsys, y0, x0, traj_len, ε=1e-4, ρ=1, max_iter=10):
         # zᵥ = 0.5 ρ ( - E xₖₜ + wₖₜ/ρ )
         Rsv = [0.5 * ρ * np.eye(E.shape[0])
                for _ in vs]
-        zsv = [0.5 * ρ * ( -E.dot(x) + w/ρ )
+        zsv = [0.5 * ρ * ( -E.dot(x) - w/ρ )
                for x, w in zip(xs_new, ws)]
 
         # v0 is fixed because x0, v_{1:T} is unknown
         v0 = E.dot(x0)
         y1 = Ay.dot(y0) + Bv.dot(v0)
 
-        # y_sys  = LinearSystem(Ay, Bv,
-        #                       Qy, np.zeros(Qy.shape[0]),
-        #                       Rsv, zsv,
-        #                       QyT, np.zeros(QyT.shape[0]), T)
-        # ys_new, vs_new, vsmin = y_sys.solve(y1, T, return_min=True)
-        # ys_new.insert(0, y0)
-        # vs_new.insert(0, v0)
-        Ps = deque([QyT])
-        os = deque([np.zeros(QyT.shape[1])])
-        Ks = deque([])
-        ks = deque([])
-        for t in reversed(range(T)):
-            Ptp1 = Ps[0]
-            Pt, ot, Kt, kt = affine_backpropagation(
-                Qy, np.zeros(Qy.shape[1]),
-                Rsv[t], zsv[t],
-                Ay, Bv, Ps[0], np.zeros(Ps[0].shape[1]))
-            Ks.appendleft(Kt)
-            ks.appendleft(kt)
-            Ps.appendleft(Pt)
-            os.appendleft(ot)
-
-        ys_new = [y0, y1]
-        vs_new = [v0]
-        for t in range(1, T):
-            yt = ys_new[t]
-            vs_new.append(Ks[t].dot(yt) + ks[t])
-            vt = vs_new[t]
-            ys_new.append(Ay.dot(yt) + Bv.dot(vt))
-        vsmin = y1.T.dot(Ps[1]).dot(y1)
-        LOG.debug("vsmin: {:.03f}".format(vsmin))
+        y_sys  = LinearSystem(Ay, Bv,
+                              Qy, np.zeros(Qy.shape[0]),
+                              Rsv, zsv,
+                              QyT, np.zeros(QyT.shape[0]), T)
+        ys_new, vs_new, vsmin = y_sys.solve(y1, T, return_min=True)
+        ys_new.insert(0, y0)
+        vs_new.insert(0, v0)
 
         ###
         # ADMM Step 3: Update Lagrange parameters
@@ -297,6 +252,7 @@ def solve_admm2(slsys, y0, x0, traj_len, ε=1e-4, ρ=0.1, max_iter=10):
     s.t.          xₜ₊₁ = Ax xₜ₊₁ + Bu uₜ
 
     """
+    warnings.warn("solve_admm2: This implementation does not provide good result. Use solve_admm instead")
     E = slsys.E
     ys, xs0, us0 = solve_seq(slsys, y0, x0, traj_len)
     vs0 = [E.dot(xt) for xt in xs0[1:]]
@@ -334,6 +290,7 @@ class SeparableLinearSystem(_SeparableLinearSystem):
         ys = [y0]
         for t, vt in enumerate(vs):
             ys.append(Ay.dot(ys[-1]) + Bv.dot(vt))
+        return ys
 
     def forward(self, y0, x0, us):
         # Generate forward trajectory
@@ -407,12 +364,12 @@ class SeparableLinearSystem(_SeparableLinearSystem):
         # minimize_v ∑ₜ yₜQₜyₜ + wₖₜᵀ(E xₖₜ - vₜ) + 0.5 ρ|E xₖₜ - vₜ|²
         # s.t.          yₜ₊₁ = Ay yₜ + Bv vₜ
         # Rᵥ = 0.5 ρ I
-        # zᵥ = 0.5 ρ ( - E xₖₜ + wₖₜ/ρ )
+        # zᵥ = 0.5 ρ ( - E xₖₜ - wₖₜ/ρ )
         xs = self.forward_x(x0, us)
         assert len(xs) == len(us) + 1
         Rsv = [0.5 * ρ * np.eye(E.shape[0])
                for _ in xs]
-        zsv = [0.5 * ρ * ( -E.dot(x) + w/ρ )
+        zsv = [0.5 * ρ * ( -E.dot(x) - w/ρ )
                for x, w in zip(xs[1:], ws)]
 
         # v0 is fixed because x0, v_{1:T} is unknown
@@ -470,7 +427,7 @@ def quadrotor_as_separable(m  = 1,
                            Ax = [[1.]],
                            y0 = [-1],
                            x0 = [0],
-                           T  = 3):
+                           T  = 100):
     Bu=[[1/m]]
     R=[[r0]]
     QyT = np.array(Qy)*100
@@ -481,7 +438,7 @@ def plot_separable_sys_results(example=quadrotor_square_example, traj_len=30):
     plotables, y0, x0, *sepsys = example()
     fig = None
     slsys = SeparableLinearSystem(*sepsys)
-    solvers = (solve_full, solve_seq, solve_admm2)
+    solvers = (solve_full, solve_seq, solve_admm)
     labels = map(attrgetter('__name__'), solvers)
     short_labels = diff_substr(labels)
     eff_traj_len = min(slsys.T, traj_len)